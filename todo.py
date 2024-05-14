8097daa30f1e6ea5469d2d0675a74b0c592a7060
akanksha-2608 <166794746+akanksha-2608@users.noreply.github.com>
2024-05-14 16:18:12 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,1 +1,32 @@
+import time
+
+class TODO:
+  
+  def _init_(self, todos, ptr):
+    self.todos todos
+    self.ptr ptr
+
+  def add(self):
+    todo - {}
+    todo['id'] self.ptr
+    self.ptr += 1
+    todo['desc'] input("Enter the description: ")
+    todo['priority'] imput("Enter Priority(high/medium/low): ")
+    todo['timestamp'] time.strftime("%Y-%m-%d %H:%M:%S")
+    todos.append(todo)
+
+  def edit(self):
+    pass
+    
+  def delete(self):
+    pass
+
+  def display(self):
+    pass
+
+  def searchbyDesc(self):
+    pass
+
+
+
 
